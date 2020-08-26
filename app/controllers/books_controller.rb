ad4bef6294240e9238a8d85190d32ef6b8e0d44c
vagrant <vagrant@localhost.localdomain>
2020-08-26 12:27:13 +0000

    [Update]edit作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,6 +18,17 @@ class BooksController < ApplicationController
 	def show
 		@book = Book.find(params[:id])
 	end
+
+	def edit
+		@book = Book.find(params[:id])
+	end
+
+	def update
+		@book = Book.find(params[:id])
+		@book.user_id = current_user.id
+		@book.update(book_params)
+		redirect_to books_path
+	end
 private
 	def book_params
 		params.require(:book).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'homes#top'
-  resources :books, only: [:new, :create, :index, :show]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :homes, only: [:top] do
     collection do
     	get 'about'
