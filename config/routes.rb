c2488850abf96496f7e23d01344b668289bf3a7c
vagrant <vagrant@localhost.localdomain>
2020-08-26 09:16:57 +0000

    [Fix]nemail認証でを認証可能に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root 'books#top'
-  resources :books, only: [:tops]
+  root 'homes#top'
+  resources :books, only: [:new]
+  resources :homes, only: [:top] do
+    collection do
+    	get 'about'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
