121be6551f9345a4790fe8c774432d59b9f8006d
saito743 <yuka.saito21@gmail.com>
2020-09-13 05:13:36 +0000

    [Update]デプロイ練習

--- config/puma.rb
+++ config/puma.rb
@@ -41,13 +41,12 @@ rails_root = Dir.pwd
 
 if Rails.env.production?
 	pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
-  state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
-  stdout_redirect(
-    File.join(rails_root, 'log', 'puma.log'),
-    File.join(rails_root, 'log', 'puma-error.log'),
-    true
+    state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
+    stdout_redirect(
+      File.join(rails_root, 'log', 'puma.log'),
+      File.join(rails_root, 'log', 'puma-error.log'),
+      true
   )
-
   daemonize
 end
 
